18e09010969fc730f5434b96a07ada17dbf3a2cb
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-10-27 08:51:41 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
-  get "homes/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about", as: "about"
 
   resources :books
   resources :users, only: [:index, :show, :update, :edit]
